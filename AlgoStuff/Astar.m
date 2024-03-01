91a035ff1db5e5285514cfbaf7c17142a19a340a
Jonathan-Dang <dangjona@gmail.com>
2024-03-01 10:52:27 -0800

    A* Algo

---
+++ AlgoStuff/Astar.m
@@ -0,0 +1,16 @@
+ss = stateSpaceSE2;
+
+ss.StateBounds = [map2D.XWorldLimits; map2D.YWorldLimits; [-pi pi]];
+
+sv = validatorOccupancyMap(ss);
+
+sv.Map = map2D;
+
+planner = plannerHybridAStar(sv, ...
+                             MinTurningRadius=4, ...
+                             MotionPrimitiveLength=6);
+
+startPose = [50 350 pi/2]; % [meters, meters, radians]
+goalPose = [550 100 -pi/2];
+refpath = plan(planner,startPose,goalPose);     
+show(planner)
