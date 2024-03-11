0a89d81c339ce7284b91bf9e8ff9cf28f0a41bca
tiguila <jtiguila7838@gmail.com>
2024-03-10 18:19:07 -0700

    Extracted x, y coordinates from A* path
    Stored them as a 2D matrix; first column x and second column y.

--- AlgoStuff/Astar.m
+++ AlgoStuff/Astar.m
@@ -1,17 +1,20 @@
+disp("A* in progress...")
+% Define your planner and other necessary objects
 ss = stateSpaceSE2;
-
 ss.StateBounds = [map2D.XWorldLimits; map2D.YWorldLimits; [-pi pi]];
-
 sv = validatorOccupancyMap(ss);
+sv.Map = map2D;
+planner = plannerHybridAStar(sv, MinTurningRadius=4, MotionPrimitiveLength=6);
 
-sv.Map = map2DClean;
-
-planner = plannerHybridAStar(sv, ...
-                             MinTurningRadius=4, ...
-                             MotionPrimitiveLength=6);
-
+% Define start and goal poses
 startPose = [50 350 pi/2]; % [meters, meters, radians]
 goalPose = [550 100 -pi/2];
-[refpath] = plan(planner,startPose,goalPose);     
-show(planner)
-"A* Complete"
+
+% Plan the path
+[refpath] = plan(planner,startPose,goalPose);
+
+% Extract x and y coordinates from the reference path and store them in a 2D matrix
+AStarPath= [refpath.States(:,1), refpath.States(:,2)];
+
+disp("A* completed!")
+
