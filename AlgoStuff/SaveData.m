e87fa913ee367be64ad2cc70ab32c77ac8918f62
tiguila <jtiguila7838@gmail.com>
2024-04-29 19:34:46 -0700

    Added documentation - SaveData.m

--- AlgoStuff/SaveData.m
+++ AlgoStuff/SaveData.m
@@ -1,8 +1,9 @@
-% This file is used to save the points the path the drone followed.
+% This file is for testing purposes only
+% This file is used to save the points in the path the drone followed - used in finding the Percent Error in the drone path.
 %% Run it ONLY after the simulation is completed!
 
 folderName = 'Testing';
-filePath = fullfile(pwd, folderName, 'PID918203.csv');
+filePath = fullfile(pwd, folderName, 'PID918226.csv');
 fileID = fopen(filePath, 'w');
 for i = 1:size(points, 1)
     fprintf(fileID, '%.15f,%.15f,%.15f\n', points(i, :));
